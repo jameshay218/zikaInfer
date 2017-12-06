b9e3ad80bafb06f44f256678e95a826e4ed99b45
jameshay218 <jameshay218@gmail.com>
2017-12-06 16:35:52 +0000

    correcting vignette folder structure

--- inst/doc/methods_pdf.R
+++
@@ -1,6 +0,0 @@
-## ----setup, echo=FALSE---------------------------------------------------
-library(kableExtra)
-library(knitr)
-options(knitr.table.format = "html")
-vital_statistics <- read.csv("vital_statistics.csv")
-
